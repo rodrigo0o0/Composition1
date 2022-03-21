2f975595eec1115389efe5c8932db39f128648e1
Rodrigo Sandeski <sandeski47@gmail.com>
2022-03-21 14:46:46 -0300

    Created Entities and Enums

---
+++ Composition1/Entities/Worker.cs
@@ -0,0 +1,58 @@
+﻿using Composition1.Entities.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Composition1.Entities
+{
+    internal class Worker
+    {
+        public string Name { get; set; }
+        public WorkLevel Level { get; set; }
+        public double BaseSalary { get; set; }
+        public Department department { get; set; }
+
+        List<HourContract> Contracts { get; set; } = new List<HourContract>();
+
+        public Worker()
+        {
+
+        }
+
+        public Worker(string name, string level, double baseSalary, Department department)
+        {
+            Name = name;
+            WorkLevel lvl;
+            Enum.TryParse<WorkLevel>(level, out lvl);
+            Level = lvl;
+            BaseSalary = baseSalary;
+            this.department = department;
+        }
+
+        public void AddContract(HourContract hc)
+        {
+            Contracts.Add(hc);
+        }
+
+        public void RemoveContract(HourContract hc)
+        {
+            Contracts.Remove(hc);
+        }
+        public double Income(int year, int month)
+        {
+            double income = BaseSalary;
+            foreach (HourContract hc in Contracts)
+            {
+                if (hc.Date.Month == month && hc.Date.Year == year)
+                {
+                    income += hc.TotalValue();
+
+                }
+            }
+            return income;
+        }
+
+    }
+}
--- Composition1/Program.cs
+++ Composition1/Program.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using Composition1.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Globalization;
 
 namespace Composition1
 {
@@ -10,6 +12,34 @@ namespace Composition1
     {
         static void Main(string[] args)
         {
+
+            Console.Write("Enter department's name: ");
+            string depName = Console.ReadLine();
+            Department department = new Department { Name = depName };
+            Console.WriteLine("Enter worker data: ");
+            Console.Write("Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Level (Junior/MidLevel/Senior) : ");
+            string level = Console.ReadLine();
+            Console.Write("Base Salary : ");
+            double baseSalary = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("How many contracts to this worker?");
+            int totalContracts = int.Parse(Console.ReadLine());
+
+            Worker worker = new Worker(name,level,baseSalary,department);
+            for (int i = 0; i < totalContracts; i++)
+            {
+                Console.WriteLine("Enter #" + (i + 1) + " contract data:");
+                Console.Write("Date (DD/MM/YYYY): ");
+                DateTime date = DateTime.Parse(Console.ReadLine());
+                Console.Write("Value per Hour: ");
+                double valuePerHour = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                Console.Write("Duration (hours) : ");
+                int duration = int.Parse(Console.ReadLine());
+
+
+
+            }
         }
     }
 }
